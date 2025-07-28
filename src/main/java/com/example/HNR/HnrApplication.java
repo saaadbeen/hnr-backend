ec540ede98c6a2ac2c0d54b47e48df87aea3ec0d
saaadbeen <155439717+saaadbeen@users.noreply.github.com>
2025-07-28 15:38:59 +0100

    Test

--- src/main/java/com/example/HNR/HnrApplication.java
+++ src/main/java/com/example/HNR/HnrApplication.java
@@ -3,9 +3,8 @@ package com.example.HNR;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-
-)public class HnrApplication {
+@SpringBootApplication
+public class HnrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HnrApplication.class, args);
